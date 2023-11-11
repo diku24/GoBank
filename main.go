e363fcff853a0fd6e09b40a0eeee0220e09d2e72
diku24 <t.diku63@gmail.com>
2023-11-12 00:08:18 +0530

    Gobank till 5 parts

--- main.go
+++ main.go
@@ -1,17 +1,50 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 )
 
+func seedAccount(store Storage, fname, lname, pw string) *Account {
+	acc, err := NewAccount(fname, lname, pw)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := store.CreateAccount(acc); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("New account Number :", acc.Number)
+	return acc
+
+}
+
+func seedAccounts(s Storage) {
+	seedAccount(s, "Diku", "GF", "Diku456")
+}
+
 func main() {
+
+	seed := flag.Bool("seed", false, "Seed the DB")
+	flag.Parse()
+
 	store, err := NewPostgressStore()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%+v\n", store)
-	// server := newApiServer(":4500", store)
-	// server.Run()
-	fmt.Println("Shri Ganeshay Namh!")
+
+	if err := store.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	if *seed {
+		fmt.Println("Seeding the database")
+		//Seed stuffs
+		seedAccounts(store)
+	}
+
+	server := newApiServer(":4500", store)
+	server.Run()
 }
--- storage.go
+++ storage.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/lib/pq"
 )
 
 type Storage interface {
-	CreatAccount(*Account) error
-	DelteAccount(int) error
+	CreateAccount(*Account) error
+	DeleteAccount(int) error
 	UpdateAccount(*Account) error
+	GetAccount() ([]*Account, error)
 	GetAccountById(int) (*Account, error)
+	GetAccountByNumber(int) (*Account, error)
 }
 
 type PostgressStore struct {
@@ -18,7 +21,7 @@ type PostgressStore struct {
 }
 
 func NewPostgressStore() (*PostgressStore, error) {
-	connStr := "user=postgres dbname=postgres password=gobank sslmode=disabled"
+	connStr := "user=postgres dbname=postgres password=password sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, err
@@ -32,4 +35,106 @@ func NewPostgressStore() (*PostgressStore, error) {
 		db: db,
 	}, nil
 
+}
+
+func (s *PostgressStore) Init() error {
+	return s.createAccontTable()
+}
+
+func (s *PostgressStore) createAccontTable() error {
+	query := `create table if not exists account (
+		id serial primary key,
+		first_name varchar(100),
+		last_name varchar(100),
+		number serial,
+		encrypted_password varchar(100),
+		balance serial,
+		created_at timestamp
+	)`
+
+	_, err := s.db.Exec(query)
+	return err
+}
+
+func (s *PostgressStore) CreateAccount(acc *Account) error {
+	query := `insert into account 
+	(first_name, last_name, number, encrypted_password, balance, created_at)
+	values ($1, $2, $3, $4, $5, $6)`
+	_, err := s.db.Query(query,
+		acc.FirstName,
+		acc.LastName,
+		acc.Number,
+		acc.EncryptedPassword,
+		acc.Balance,
+		acc.CreatedAt)
+	//_, err := s.db.Query(query, acc.FirstName, acc.LastName, acc.Balance, acc.EncryptedPassword, acc.Number, acc.CreatedAt)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *PostgressStore) UpdateAccount(*Account) error {
+	return nil
+}
+
+func (s *PostgressStore) DeleteAccount(id int) error {
+	_, err := s.db.Query("delete from account where id = $1", id)
+	return err
+}
+
+func (s *PostgressStore) GetAccountByNumber(number int) (*Account, error) {
+	rows, err := s.db.Query("select * from account where number = $1", number)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		return scanIntoAccounts(rows)
+	}
+	return nil, fmt.Errorf("Account with number  %d not Found", number)
+}
+func (s *PostgressStore) GetAccountById(id int) (*Account, error) {
+	rows, err := s.db.Query("select * from account where id = $1", id)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		return scanIntoAccounts(rows)
+	}
+	return nil, fmt.Errorf("Account %d not Found", id)
+}
+
+func (s *PostgressStore) GetAccount() ([]*Account, error) {
+	rows, err := s.db.Query(`select * from account`)
+	if err != nil {
+		return nil, err
+	}
+
+	accounts := []*Account{}
+
+	for rows.Next() {
+		account, err := scanIntoAccounts(rows)
+		if err != nil {
+			return nil, err
+		}
+		accounts = append(accounts, account)
+
+	}
+	return accounts, nil
+}
+
+func scanIntoAccounts(rows *sql.Rows) (*Account, error) {
+	account := new(Account)
+	err := rows.Scan(
+		&account.ID,
+		&account.FirstName,
+		&account.LastName,
+		&account.Number,
+		&account.EncryptedPassword,
+		&account.Balance,
+		&account.CreatedAt)
+
+	return account, err
 }
